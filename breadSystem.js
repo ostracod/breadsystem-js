414d4d84b00e5c0999f193a7126df92fcf7271c5
John Eisenmann <esperantanaso@gmail.com>
2019-05-19 16:01:05 -0400

    Parse instruction from bytecode file

--- breadSystem.js
+++ breadSystem.js
@@ -5,7 +5,10 @@ var pathUtils = require("path");
 var primaryVolumeNativePath = "./primaryVolume";
 var bootAppsPath = ":bootApps";
 
-var agentList = [];
+var argumentAmountMap = {};
+argumentAmountMap[0x0900] = 3; // add
+
+var runningAgentList = [];
 
 function joinPath(tail, head) {
     if (tail.length > 0 && tail.substring(tail.length - 1, tail.length) == "/") {
@@ -121,6 +124,7 @@ function BytecodeFile(path) {
         readU32(content, 8),
     ];
     this.functionList = [];
+    this.entryPointFunction = null;
     var topLevelRegionList = parseBytecodeFileRegionList(content, 12);
     var index = 0;
     while (index < topLevelRegionList.length) {
@@ -138,15 +142,103 @@ BytecodeFile.prototype.parseFunctionsRegion = function(region) {
     while (index < functionRegionList.length) {
         var tempRegion = functionRegionList[index];
         if (tempRegion.type == 0) {
-            this.functionList.push(new EntryPointFunction(tempRegion));
+            var tempFunction = new EntryPointFunction(tempRegion);
+            this.entryPointFunction = tempFunction;
         }
+        this.functionList.push(tempFunction);
         index += 1;
     }
 }
 
+function ConstantArgument(value) {
+    this.value = value;
+}
+
+ConstantArgument.prototype.read = function() {
+    return this.value;
+}
+
+function GlobalFrameArgument(dataType, index) {
+    this.dataType = dataType;
+    this.index = index;
+}
+
+GlobalFrameArgument.prototype.read = function() {
+    // TODO: Implement.
+    
+}
+
+GlobalFrameArgument.prototype.write = function(value) {
+    // TODO: Implement.
+    
+}
+
 function Agent(appPath) {
     this.bytecodeFile = new BytecodeFile(appPath);
-    agentList.push(this);
+    runningAgentList.push(this);
+    // Offset within the instruction array.
+    this.instructionOffset = 0;
+    this.currentFunction = this.bytecodeFile.entryPointFunction;
+}
+
+
+Agent.prototype.parseInstructionArgument = function() {
+    var bytecodeBuffer = this.currentFunction.buffer;
+    var baseOffset = this.currentFunction.instructionArrayOffset;
+    var prefix = readU8(bytecodeBuffer, baseOffset + this.instructionOffset);
+    this.instructionOffset += 1;
+    var referenceType = (prefix & 0xF0) >> 4;
+    var dataType = prefix & 0x0F;
+    // Constant value.
+    if (referenceType == 0) {
+        var tempValue;
+        if (dataType == 1) {
+            tempValue = readU8(bytecodeBuffer, baseOffset + this.instructionOffset);
+            this.instructionOffset += 1;
+        }
+        return new ConstantArgument(tempValue);
+    }
+    // Global frame.
+    if (referenceType == 1) {
+        var tempIndexArgument = this.parseInstructionArgument();
+        var frameIndex = tempIndexArgument.read();
+        return new GlobalFrameArgument(dataType, frameIndex);
+    }
+    return null;
+}
+
+Agent.prototype.performNextInstruction = function() {
+    if (this.instructionOffset >= this.currentFunction.instructionArraySize) {
+        this.terminate();
+        return;
+    }
+    var bytecodeBuffer = this.currentFunction.buffer;
+    var baseOffset = this.currentFunction.instructionArrayOffset;
+    var opcode = readU16(bytecodeBuffer, baseOffset + this.instructionOffset);
+    this.instructionOffset += 2;
+    var argumentAmount = argumentAmountMap[opcode];
+    if (typeof argumentAmount === "undefined") {
+        console.log("ERROR: Unrecognized opcode! (" + opcode + ")");
+        this.terminate();
+        return;
+    }
+    var argumentList = [];
+    while (argumentList.length < argumentAmount) {
+        var tempArgument = this.parseInstructionArgument();
+        argumentList.push(tempArgument);
+    }
+    if (opcode == 0x0900) {
+        // TODO: Perform the instruction.
+        console.log(argumentList);
+        
+    }
+}
+
+Agent.prototype.terminate = function() {
+    var index = runningAgentList.indexOf(this);
+    if (index >= 0) {
+        runningAgentList.splice(index, 1);
+    }
 }
 
 function launchApp(path) {
@@ -176,8 +268,19 @@ function launchBootApps() {
     }
 }
 
-launchBootApps();
+function timerEvent() {
+    if (runningAgentList.length <= 0) {
+        clearInterval(timerInterval);
+    }
+    var index = runningAgentList.length - 1;
+    while (index >= 0) {
+        var tempAgent = runningAgentList[index];
+        tempAgent.performNextInstruction();
+        index -= 1;
+    }
+}
 
-// TODO: Execute instructions.
+launchBootApps();
+var timerInterval = setInterval(timerEvent, 50);
 
 
